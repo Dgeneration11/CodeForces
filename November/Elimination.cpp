c728f228899be1d8138b6008ab64b228b44d2c23
Bunty Dgeneration <buntydgeneration@gmail.com>
2020-11-05 15:11:39 +0530

    Eliminaion

---
+++ November/Elimination.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    for(int i=0; i<t;i++){
+        int a, b, c, d;
+        cin>>a>>b>>c>>d;
+        cout<<max(a+b, c+d)<<endl;
+    }
+}
